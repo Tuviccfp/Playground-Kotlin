17a62aa28fd5c0d98cf4fc3709986876d0a8254e
Victor <victorcordeirofp@gmail.com>
2024-09-04 16:14:43 -0300

    Refatoração de ClientService com as exceptions

--- src/main/kotlin/com/example/demo/domain/Client.kt
+++ src/main/kotlin/com/example/demo/domain/Client.kt
@@ -3,10 +3,11 @@ package com.example.demo.domain
 import lombok.EqualsAndHashCode
 import lombok.Getter
 import lombok.Setter
+import org.jetbrains.annotations.NotNull
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import java.io.Serializable
 
 @Document(collection = "users")
-data class Client (@Id val id: String?, var name: String, var email: String): Serializable {
+data class Client (@Id var id: String?, @NotNull var name: String, @NotNull var email: String): Serializable {
 }
--- src/main/kotlin/com/example/demo/services/ClientService.kt
+++ src/main/kotlin/com/example/demo/services/ClientService.kt
@@ -1,33 +1,45 @@
 package com.example.demo.services
 
 import com.example.demo.domain.Client
+import com.example.demo.exceptions.CreateExpection
+import com.example.demo.exceptions.DeleteException
+import com.example.demo.exceptions.UpdateException
 import com.example.demo.repository.ClientRepository
-import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException
-import org.springframework.data.mongodb.core.aggregation.BooleanOperators.Not
 import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
 class ClientService (private val clientRepository: ClientRepository)  {
     fun findAllClients(): List<Client> {
+        if (clientRepository.findAll().isEmpty()) {
+            throw Exception()
+        }
         return clientRepository.findAll();
     }
 
     fun findClientById(id: String): Client? {
-        return clientRepository.findById(id).orElse(null);
+        return clientRepository.findById(id).orElseThrow { com.example.demo.exceptions.NotFoundException("Client with id: $id") };
     }
-
+            
     fun save(client: Client): Client {
+        if (client.equals(null)) {
+            throw CreateExpection("Erro ao tentar salvar um novo usuário")
+        }
         return clientRepository.save(client);
     }
 
     fun update(id: String, client: Client): Client {
         if (findClientById(id) != null) {
-            throw NotFoundException()
+            throw com.example.demo.exceptions.NotFoundException("Client with id: $id")
+        } else if(client.equals(null)) {
+            throw UpdateException("Erro ao tentar atualizar os dados")
         }
-            return clientRepository.save(client);
+        return clientRepository.save(client);
     }
     fun delete(id: String) {
+        if (findClientById(id) == null) {
+            throw DeleteException("Erro ao localizar o id e deletar.")
+        }
         clientRepository.deleteById(id);
     }
 }
